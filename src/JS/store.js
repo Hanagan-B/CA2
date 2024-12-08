aac032a32cd1a1b4ab5930d7a1de59a8eb68f24c
Vanessa <vanessa.tamboril.ara@gmail.com>
2024-12-08 15:27:14 +0000

    vanessa set img

--- src/JS/store.js
+++ src/JS/store.js
@@ -1,11 +1,10 @@
-// Função para exibir o botão "Compre agora" ao rolar a página
+
 window.addEventListener('scroll', function() {
     var buyNowButton = document.getElementById('buy-now');
     
-    // Verifica se a rolagem passou de 100px
+    
     if (window.scrollY > 100) {
-      buyNowButton.style.display = 'block';  // Exibe o botão
+      buyNowButton.style.display = 'block';  
     } else {
-      buyNowButton.style.display = 'none';  // Esconde o botão
-    }
+      buyNowButton.style.display = 'none'; 
 });
