a0ba54396b5e9e0adfe32fa07b791a4d1c89b71e
Bruna <bruna.oliveira.roraima@gmai.com>
2024-12-08 12:52:12 +0000

    set changes

--- src/JS/mobile-nav-js.js
+++ src/JS/mobile-nav-js.js
@@ -1,7 +1,7 @@
-// Script para o menu móvel
+
 const mobileBtn = document.getElementById('mobile-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
 mobileBtn.addEventListener('click', function() {
-    mobileMenu.classList.toggle('active'); // Alterna a visibilidade do menu
+    mobileMenu.classList.toggle('active'); 
 });
